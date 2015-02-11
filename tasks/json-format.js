28bbd74f5812c6f7a50295a291f55cba7d36e3ee
Jahed <jahed.public@gmail.com>
2015-02-11 01:15:05 +0000

    Refactored task

--- tasks/json-format.js
+++ tasks/json-format.js
@@ -1,59 +1,53 @@
-//
-// grunt-json-min
-// https://github.com/jahed/grunt-json-min
-//
-// Copyright (c) 2014 Jahed Ahmed
-// Licensed under the MIT license.
-//
-
-'use strict';
-
 module.exports = function(grunt) {
-
-    grunt.registerMultiTask('json-format', 'A Grunt Task for formatting JSON files.', function() {
-        var options = this.options({
+    grunt.registerMultiTask(
+        'json-format',
+        'A Grunt Task for formatting JSON files.',
+        function() {
+            var options,
+                succeededFiles;
+    
+            options = this.options({
                 indent: null,
                 remove: []
-            }),
-            successCount = 0;
-
-        function removeReplacer(key, value) {
-            if(options.remove.indexOf(key) !== -1) {
-                return undefined;
-            }
-
-            return value;
-        }
-
-        this.files.forEach(function(f) {
-            f.src.filter(function(filepath) {
-                if (!grunt.file.exists(filepath)) {
-                    grunt.fail.warn('Source file "' + filepath + '" not found.');
-                    return false;
-                }
-                return true;
-            }).forEach(function(filepath) {
-                var json;
-
-                try {
-                    json = grunt.file.readJSON(filepath);
-                } catch(e) {
-                    grunt.fail.warn(e);
-                    return;
-                }
-
-                grunt.log.verbose.write('Formatting ' + filepath + ' to ' + f.dest + '...');
-
-                grunt.file.write(f.dest, JSON.stringify(json, removeReplacer, options.indent));
-
-                grunt.log.verbose.ok();
-                successCount++;
             });
-        });
 
-        grunt.log.oklns(
-            successCount + ' JSON ' + grunt.util.pluralize(successCount, 'file/files') + ' formatted.'
-        );
-    });
+            succeededFiles = this.files.filter(function(fileOptions) {
+                return fileOptions.src
+                    .filter(exists)
+                    .filter(function process(filepath) {
+                        var parsedObject,
+                            processedJSON;
 
+                        try {
+                            parsedObject = grunt.file.readJSON(filepath);
+                            grunt.log.verbose.write('Formatting ' + filepath + ' to ' + fileOptions.dest + '...');
+                            processedJSON = JSON.stringify(parsedObject, removePropertyReplacer, options.indent);
+                            grunt.file.write(fileOptions.dest, processedJSON);
+                            grunt.log.verbose.ok();
+                            return true;
+                        } catch(e) {
+                            grunt.fail.warn(e);
+                            return false;
+                        }
+                    });
+            });
+    
+            grunt.log.oklns(succeededFiles.length + ' JSON ' + grunt.util.pluralize(succeededFiles.length, 'file/files') + ' formatted.');
+        
+            function removePropertyReplacer(key, value) {
+                if(options.remove.indexOf(key) !== -1) {
+                    return undefined;
+                }
+                return value;
+            }
+            
+            function exists(path) {
+                var fileExists = grunt.file.exists(path);
+                if (!fileExists) {
+                    grunt.fail.warn('Source file "' + path + '" not found.');
+                }
+                return fileExists;
+            }
+        }
+    );
 };
